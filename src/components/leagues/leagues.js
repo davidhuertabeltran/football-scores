7e3c6d1cbd16622bb520df9d9afce58d3b8e71eb
David Huerta <huerta.beltran.david@gmail.com>
2024-12-31 13:10:24 +0100

    move API calls to fixtures table component

--- src/components/leagues/leagues.js
+++ src/components/leagues/leagues.js
@@ -1,99 +1,50 @@
-import { useParams } from 'react-router-dom';
-import { useEffect, useState, useCallback, useMemo } from 'react';
-import { FetchMatches } from '../../requests/fetch-matches';
-import { finishedDummy } from '../../lib/finished-matches-dummy';
-import { Back } from '../buttons/back';
-import FixturesTable from '../fixtures-table/fixtures-table';
-import { useRef } from 'react';
-import { leaguesListAllData } from '../../lib/leagues-list-all-data';
-import { data } from '../../lib/dummy-data';
+import { useParams } from "react-router-dom";
+import { useMemo, useRef } from "react";
 
+import { Back } from "../buttons/back";
+import FixturesTable from "../fixtures-table/fixtures-table";
+import { leaguesListAllData } from "../../lib/leagues-list-all-data";
 
-export const Leagues = ({ fixtures }) => {
-	const toTopRef = useRef(null);
-	const { leagueID } = useParams();
+export const Leagues = () => {
+  const toTopRef = useRef(null);
+  const { leagueID } = useParams();
 
+  const currentLeague = useMemo(
+    () =>
+      leaguesListAllData.find((l) => l.league.id === parseInt(leagueID, 10)),
+    [leagueID]
+  );
 
-	const leagues = fixtures.filter(fixture => fixture.league.id === parseInt(leagueID));
+  if (!currentLeague) {
+    return <div className="text-center">League not found</div>;
+  }
 
-	const urlFinishedFixtures = useMemo(() => `https://api-football-v1.p.rapidapi.com/v3/fixtures?league=${leagueID}&last=10&status=FT-AET-PEN`, [leagueID]);
-	const urlLiveFixtures = useMemo(() => `https://api-football-v1.p.rapidapi.com/v3/fixtures?live=all&league=${leagueID}`, [leagueID]);
+  const { name, logo } = currentLeague.league;
 
-	const [liveFixtures, setLiveFixtures] = useState([]);
-	const [finishedFixtures, setFinishedFixtures] = useState([]);
-	const [loading, setLoading] = useState(false); //change to true when using API
-
-	const currentLeague = useMemo(() => leaguesListAllData.find(l => l.league.id === parseInt(leagueID, 10)), [leagueID]);
-
-	const fetchLiveData = useCallback(async () => {
-		try {
-			const matches = await FetchMatches(urlLiveFixtures);
-			setLiveFixtures(matches);
-		} catch (error) {
-			console.error('Error fetching live matches:', error);
-		}
-	}, [urlLiveFixtures]);
-
-	const fetchFinishedData = useCallback(async () => {
-		try {
-			const matches = await FetchMatches(urlFinishedFixtures);
-			setFinishedFixtures(matches);
-		} catch (error) {
-			console.error('Error fetching finished matches:', error);
-		}
-	}, [urlFinishedFixtures]);
-
-	//comment when NOT using API
-	// useEffect(() => {
-	// 	setLoading(true);
-	// 	Promise.all([fetchLiveData(), fetchFinishedData()]).finally(() => {
-	// 		setLoading(false);
-	// 		window.scrollTo(0, 0);
-	// 	});
-	// }, [fetchLiveData, fetchFinishedData]);
-
-	if (!currentLeague) {
-		return <div className="text-center">League not found</div>;
-	}
-
-	const { name, logo } = currentLeague.league;
-
-	console.log('leagues', leagues);
-
-	return (
-		<div className="league-container" ref={toTopRef}>
-			<div className="league-details flex justify-between items-center px-10">
-				<Back />
-				<div className="league-container flex items-center gap-2">
-					<div className="league-logo">
-						<img className="logo" src={logo} alt={name} width={40} />
-					</div>
-					<div className="league-name">
-						<p className="text-right text-sm font-bold">{name}</p>
-					</div>
-				</div>
-			</div>
-			<div className="live-matches-league-container border rounded-xl px-6 py-12 mt-8">
-				<p className="text-xl font-bold uppercase text-center">Live Matches</p>
-				{loading ? (
-					<div className="text-center">Loading...</div>
-				) : leagues.length === 0 ? ( //replace to leagues when not using API, use liveFixtures when using API
-					<div className="text-center">There are no live matches at the moment</div>
-				) : (
-					<FixturesTable fixtures={leagues} /> //replace to leagues when not using API, and use liveFixtures when using API
-				)}
-			</div>
-			<div className="finished-matches-league-container border rounded-xl px-6 py-12 mt-8">
-				<p className="text-l font-bold text-center uppercase">Finished matches</p>
-				<p className="text-l font-bold text-center uppercase">{name}</p>
-				{loading ? (
-					<div className="text-center">Loading...</div>
-				) : finishedDummy.length === 0 ? (
-					<div className="text-center">There are no data for previous matches at the moment</div>
-				) : (
-					<FixturesTable fixtures={finishedDummy} /> //change to finishedDummy when not using API, change to finishedFixtures when using API
-				)}
-			</div>
-		</div>
-	);
+  return (
+    <div className="league-container" ref={toTopRef}>
+      <div className="league-details flex justify-between items-center px-10">
+        <Back />
+        <div className="league-container flex items-center gap-2">
+          <div className="league-logo">
+            <img className="logo" src={logo} alt={name} width={40} />
+          </div>
+          <div className="league-name">
+            <p className="text-right text-sm font-bold">{name}</p>
+          </div>
+        </div>
+      </div>
+      <div className="live-matches-league-container border rounded-xl px-6 py-12 mt-8">
+        <p className="text-xl font-bold uppercase text-center">Live Matches</p>
+        <FixturesTable leagueID={leagueID} />
+      </div>
+      <div className="finished-matches-league-container border rounded-xl px-6 py-12 mt-8">
+        <p className="text-l font-bold text-center uppercase">
+          Finished matches
+        </p>
+        <p className="text-l font-bold text-center uppercase">{name}</p>
+        <FixturesTable leagueID={leagueID} isFinished={true} />
+      </div>
+    </div>
+  );
 };
